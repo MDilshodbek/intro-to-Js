182d1abe0a8c2c92f557ac2e3ce6781b30ceb803
MDilshodbek <mdbek55@gmail.com>
2024-01-31 21:52:53 +0900

    diving into Js

---
+++ index.js
@@ -0,0 +1,93 @@
+// String types
+// let single_qoutes = '"Hello" me';
+// let double_quotes = "'Hello' me";
+// let backtick = `'Hello' me`;
+
+// Exceptions
+
+// let backtick = `\`Hello\` me`;
+
+// console.log(single_qoutes, double_quotes, backtick);
+
+// var name = "Dilshodbek";
+// var surname = "Makhmudov";
+// var middlename = "'David'";
+
+// console.log(name + surename + middlename);
+
+// ${}
+
+// var nickname = "David";
+// var fullname = `Dilshodbek ${nickname} Makhmudov`;
+
+// console.log(fullname);
+
+// console.log(fullname[3]);
+
+// Length
+
+// var nickname = "apperance";
+
+// console.log(nickname.length);
+// console.log(nickname[3]);
+
+// Concat
+
+// var name = "Dilshodbek";
+// var surname = "Makhmudov";
+// var middlename = "'David'";
+
+// console.log(surname.concat(middlename));
+
+//No difference -[]&charAt
+
+// console.log(surname[3]);
+// console.log(surname.charAt(3));
+
+// Replace
+
+// var name = "Dilshodbek Makhmudov";
+
+// console.log(name.replace("Dilshodbek", "David"));
+
+// var fullname = "Dilshodbek Makhmudov Dilshodbek Dilshodbek";
+
+// console.log(fullname.replaceAll("Dilshodbek", "David"));
+
+// Split
+
+// is used to change the string info into array
+
+// var name = "Dilshodbek Makhmudov";
+
+// console.log(name.split());
+// console.log(name.split(" "));
+
+// // repeat
+
+// var name = "Dilshodbek Makhmudov";
+
+// console.log(name.repeat(3));
+
+// startsWith
+// endsWith
+
+// var name = "Dilshodbek Makhmudov";
+
+// console.log(name.startsWith("Dilshodbek"));
+// console.log(name.endsWith("Makhmudov"));
+
+// ToLowerCase
+// TOUpperCase
+
+// var name = "Dilshodbek Makhmudov";
+
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+
+// Trim - to remove the free space
+
+var name = "    Dilshodbek Makhmudov";
+
+console.log(name);
+console.log(name.trim());
